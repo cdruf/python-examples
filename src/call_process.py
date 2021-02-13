41039ce665b580d837421261a573e737c61b7b8e
christian.ruf <christian.ruf@bain.com>
2021-02-13 22:00:55 +0100

    Cleaned up call_process

--- src/call_process.py
+++ src/call_process.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 10 10:51:58 2020
-
-"""
+import os
 import subprocess
 
+"""
+This example script calls various external processes.
 
-#%%
-# with the run function (recommended)
+* ls
+* A Java-process with arguments
+
+"""
+print(os.getcwd())
+jar_path = 'resources/process_java_example.jar'
+
+# %%
+
+"""
+# With the run function (recommended)
+"""
 
 subprocess.run(['ls', '-l'])
 
-subprocess.run(['java',  '-jar', 'process_java_example.jar', 'arg0'])
+subprocess.run(['java', '-jar', jar_path, 'arg0'])
 
-subprocess.run(['java',  '-jar', 'process_java_example.jar', 'arg0'], 
+subprocess.run(['java', '-jar', jar_path, 'arg0'],
                stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
+# %%
 
-#%%
-# with Popen 
+"""
+# With Popen 
+"""
 
-process = subprocess.Popen(['java',  '-jar', 'process_java_example.jar', 'arg0'])
+process = subprocess.Popen(['java', '-jar', jar_path, 'arg0'])
 process.communicate()
 
-#%%
 # there are more ways to do it
